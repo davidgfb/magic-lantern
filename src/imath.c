3b82921de0c9728ed761678f8e388a6c580c6737
davidgfb <davidgracia99@gmail.com>
2021-03-26 12:40:40 +0100

    qr

--- src/imath.c
+++ src/imath.c
@@ -3,36 +3,39 @@
 #include "dryos.h"
 #include "math.h"
 
-int powi(int base, int power)
-{
+int powi(int base, int power) {
     int result = 1;
-    while (power)
-    {
-        if (power & 1)
+    while (power) {
+        if (power & 1) {
             result *= base;
+        }
         power >>= 1;
         base *= base;
     }
     return result;
 }
 
-int log2i(int x)
-{
+int log2i(int x) {
     int result = 0;
-    while (x >>= 1) result++;
+    while (x >>= 1) {
+    	result++;
+    }
     return result;
 }
 
-int log10i(int x)
-{
+int log10i(int x) {
     int result = 0;
-    while(x /= 10) result++;
+    while(x /= 10) { 
+    	result++;
+    }
     return result;
 }
 
 /* todo: integer-only implementation? */
-uint32_t log_length(int v)
-{
-    if (!v) return 0;
-    return (unsigned int)(log2f(v) * 100);
+uint32_t log_length(int v) {
+	uint32_t value = 0; 
+    if (v) {
+    	value = ((unsigned int)(log2f(v) * 100));
+    }
+    return value;
 }
