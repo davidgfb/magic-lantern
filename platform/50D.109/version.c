26595511e35fd24e42ddf3bb6c96c002c91ac68d
davidgfb <davidgracia99@gmail.com>
2021-03-27 12:46:15 +0100

    cc

--- platform/50D.109/version.c
+++ platform/50D.109/version.c
@@ -1,4 +1,4 @@
 const char build_version[] = "Nightly.2021Mar27.50D109";
 const char build_id[] = "352a05ff20db+ (qemu)";
-const char build_date[] ="2021-03-27 11:37:21 UTC";
+const char build_date[] ="2021-03-27 11:44:29 UTC";
 const char build_user[] = "david@david-VirtualBox";
